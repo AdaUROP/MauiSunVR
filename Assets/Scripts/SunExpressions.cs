171c75c44492ca5c44ec55f25ded72b33a20cb02
dchan3 <dchan3@hawaii.edu>
2016-10-10 00:42:37 -1000

    Started defining sun expression constants

---
+++ Assets/Scripts/SunExpressions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunExpressions {
+	public static SunEmotion mergeExpressions(SunEmotion one, SunEmotion two) {
+		return new SunEmotion (
+			Mathf.Min(one.getSmiling () + two.getSmiling (), 100f), 
+			Mathf.Min(one.getAngryEyes () + two.getAngryEyes (), 100f),
+			Mathf.Min(one.getSad () + two.getSad (), 100f), 
+			Mathf.Min(one.getClosed () + two.getClosed (), 100f), 
+			Mathf.Min(one.getPursed () + two.getPursed (), 100f),
+			Mathf.Min(one.getAngry () + two.getAngry (), 100f)); 
+	}
+
+	public static readonly SunEmotion LIPS_EE = new SunEmotion(50f, 0f, 0f, 0f, 100f, 0f);
+
+	public static readonly SunEmotion EXPR_ANGRY = new SunEmotion(0f, 100f, 0f, 0f, 0f, 100f);
+}
