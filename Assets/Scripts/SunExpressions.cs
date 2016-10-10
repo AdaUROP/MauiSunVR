59eea384be4cde17753779d1ab2dab523899244d
dchan3 <dchan3@hawaii.edu>
2016-10-10 11:28:44 -1000

    Implemented sun emotion queue

--- Assets/Scripts/SunExpressions.cs
+++ Assets/Scripts/SunExpressions.cs
@@ -12,7 +12,17 @@ public class SunExpressions {
 			Mathf.Min(one.getAngry () + two.getAngry (), 100f)); 
 	}
 
+	public static readonly SunEmotion LIPS_AH = new SunEmotion (100f, 0f, 0f, 100f, 100f, 100f);
 	public static readonly SunEmotion LIPS_EE = new SunEmotion(50f, 0f, 0f, 0f, 100f, 0f);
+	public static readonly SunEmotion LIPS_EH = new SunEmotion (50f, 0f, 0f, 50f, 75f, 0f);
+	public static readonly SunEmotion LIPS_O = new SunEmotion (0f, 0f, 0f, 0f, 100f, 0f);
 
-	public static readonly SunEmotion EXPR_ANGRY = new SunEmotion(0f, 100f, 0f, 0f, 0f, 100f);
+	public static readonly SunEmotion LIPS_MBP = new SunEmotion (0f, 0f, 0f, 100f, 100f, 0f);
+	public static readonly SunEmotion LIPS_SS = new SunEmotion(0f, 0f, 0f, 100f, 0f, 0f);
+	public static readonly SunEmotion LIPS_FV = new SunEmotion (0f, 0f, 0f, 75f, 75f, 0f);
+	public static readonly SunEmotion LIPS_DEF = new SunEmotion (0f, 0f, 0f, 50f, 75f, 0f);
+
+	public static readonly SunEmotion EXPR_ANGRY = new SunEmotion(0f, 100f, 0f, 0f, 0f, 0f);
+	public static readonly SunEmotion EXPR_SCARED = new SunEmotion(0f, 0f, 100f, 0f, 0f, 0f);
+	public static readonly SunEmotion EXPR_DISGUSTED = new SunEmotion(0f, 50f, 50f, 0f, 0f, 0f);
 }
